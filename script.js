da0bab47bf20110456303291cbe0ddd33e11375e
Jenina <jeninadelacruz521@yahoo.com>
2024-02-19 19:16:01 -0800

    added prompts and variables

--- script.js
+++ script.js
@@ -1,82 +1,71 @@
-// Assignment code here
 
+// Assignment code here
 var lowerC = "abcdefghijklmnopqrstuvwxyz";
 var upperC = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var numC = "1234567890";
 var specialC = "!@#$%^&*()_-+=";
 var allChar = lowerC.concat(upperC).concat(numC).concat(specialC);
-
-//var numbersLetters = lowerC.concat(numC);
-
-
-//computer guess(random numbers for letters and numbers)
-var letterLower = Math.floor(Math.random() * lowerC.length);
-var letterUpper = Math.floor(Math.random() * upperC.length);
-var numnumC = Math.floor(Math.random() * numC.length);
-var specChars = Math.floor(Math.random() * numC.length);
-
-//random picks by computer
-var randLower = lowerC[letterLower];
-var randUpper = upperC[letterUpper];
-var randNum = numC[numnumC];
-var randSpecial = specialC[specChars];
-
-
-// Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
-function generatePassword(lowerCase, upperCase, specChars, userLength)
+function generatePassword()
 {
-//filter whats included
-var numOptions = lowerCase + upperCase + specChars + userLength;
-console.log(numOptions);
+  var password = ('');
+  var userLength
+  var lengthAlert
+  
+
+userLength = +prompt("How long would you want your password to be?")
+  if (userLength <8 || userLength >128){
+    do {
+    lengthAlert = +prompt("at least 8 characters and no more than 128 characters");
+    userLength = lengthAlert;
+    } 
+    while (lengthAlert >= 129 || lengthAlert <= 7)};
+    console.log(userLength);
+    console.log(typeof userLength);
+
+  var lowerCase = confirm("Do you want to include lowerCase?")
+   console.log(lowerCase)
+  
+  var upperCase = confirm("Do you want to include upperCase?")
+   console.log(upperCase);
+
+  var numCase = confirm("Do you want to include numbers?")
+   console.log(numCase);
+
+  var specChars = confirm("Do you want to include special characters");
+   console.log(specChars)
+  
+  var selectedChars = ('');
+    selectedChars += lowerCase ? lowerC : " ";
+    selectedChars += upperCase ? upperC : " ";
+    selectedChars += numCase ? numC : " ";
+    selectedChars += specChars ? specialC : " ";
+      if (selectedChars == 0)
+      {
+        alert("Must select at least 1 set of characters!")
+      };
+      
+      
+
+  for(var i = 0; i < userLength; i++)
+    {
+    randomSelected = Math.floor(Math.random() * selectedChars.length);
+    password += selectedChars[randomSelected]
+    }
+
+  var passwordText = document.querySelector("#password");
+    passwordText.value = password;
+    console.log(password); 
+  
 }
 
 // Write password to the #password input
-function writePassword() {
-  var password = generatePassword(randLower, 
-                                  randUpper, 
-                                  randNum, 
-                                  randSpecial,
-                                  userLength);
-  var passwordText = document.querySelector("#password");
-
-
-  var userLength
-  var lengthAlert
-
-userLength = prompt("How long would you want your password to be?")
-if (userLength <8 || userLength >129){
-  do {
-    lengthAlert = prompt("at least 8 characters and no more than 128 characters");
-  } while (lengthAlert >= 129 || lengthAlert <= 7)};
-  // console.log(userLength);
-
-  
-  var lowerCase = confirm("Do you want to include lowerCase?")
-  console.log(lowerCase)
-  if(lowerCase == true){
-    passwordText += letterLower
-  }
-
-  
-  var upperCase = confirm("Do you want to include upperCase?")
-  console.log(upperCase);
-
-  
-  var specChars = confirm("Do you want to include special characters");
-  console.log(specChars)
-
-  
-  
-
-
-  passwordText.value = password;
-  
-
-  
+function writePassword() 
+{
+  password
+  var password = generatePassword();
 }
 
-writePassword
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
